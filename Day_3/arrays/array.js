f7158908263b2cff46fb5684b684e3a075de8cff
ShubhamBhurke <shubhambhurke123@gmail.com>
2020-04-28 15:02:16 +0530

    day3 commits

---
+++ Day_3/arrays/array.js
@@ -0,0 +1,47 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    //var sorted=nums.sort()
+    var no_dups = nums.filter((a, b) => nums.indexOf(a) === b)
+    //return no_dups
+    var final=no_dups.sort(function(x, y) { return x < y; } )
+    return final[1]
+    //var second_large=final[final.length-2]
+    //return second_large
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
